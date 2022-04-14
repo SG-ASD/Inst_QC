2bd87262c44b7d7c74c6f5e77fb3104ea6e0841c
tlsgn511 <shlee2@seegene.com>
2022-04-14 14:32:38 +0900

    22/04/14 업데이트
    22/04/14 업데이트

--- Appearanceapp/forms.py
+++ Appearanceapp/forms.py
@@ -6,4 +6,5 @@ from Appearanceapp.models import Appearance
 class AppearanceCreationForm(ModelForm):
     class Meta:
         model = Appearance
-        fields = ['Instrument_SN', 'Shock_Watch', 'Binding', 'Labels', 'Packaging_Box', 'Wooden_Pallet','Transport_Jig']
+        fields = ['Instrument_SN', 'Shock_Watch', 'Binding', 'Labels', 'Packaging_Box', 'Wooden_Pallet','Transport_Jig',
+        'Shock_Watch_Image', 'Binding_Image', 'Labels_Image', 'Packaging_Box_Image', 'Wooden_Pallet_Image', 'Transport_Jig_Image', 'Video']
--- Appearanceapp/models.py
+++ Appearanceapp/models.py
@@ -18,12 +18,20 @@ class Appearance(models.Model):
     Packaging_Box = models.CharField(max_length=20, null=True)
     Wooden_Pallet = models.CharField(max_length=20, null=True)
     Transport_Jig = models.CharField(max_length=20, null=True)
-    Front = models.CharField(max_length=20, null=True)
-    Right = models.CharField(max_length=20, null=True)
-    Left = models.CharField(max_length=20, null=True)
-    Back = models.CharField(max_length=20, null=True)
-    Acc_Damage = models.CharField(max_length=20, null=True)
-    Acc_Missing = models.CharField(max_length=20, null=True)
+    Shock_Watch_Image = models.ImageField(upload_to='Appearance/', null=True)
+    Binding_Image = models.ImageField(upload_to='Appearance/', null=True)
+    Labels_Image = models.ImageField(upload_to='Appearance/', null=True)
+    Packaging_Box_Image = models.ImageField(upload_to='Appearance/', null=True)
+    Wooden_Pallet_Image = models.ImageField(upload_to='Appearance/', null=True)
+    Transport_Jig_Image = models.ImageField(upload_to='Appearance/', null=True)
+    Video = models.FileField(upload_to="video/", null=True)
+    # Front = models.CharField(max_length=20, null=True)
+    # Right = models.CharField(max_length=20, null=True)
+    # Left = models.CharField(max_length=20, null=True)
+    # Back = models.CharField(max_length=20, null=True)
+    # Acc_Damage = models.CharField(max_length=20, null=True)
+    # Acc_Missing = models.CharField(max_length=20, null=True)
+
 
 
 
