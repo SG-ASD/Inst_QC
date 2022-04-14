290f89ba0d9dbf4693333d00fdd9ffab507e751d
ch8638 <ch8638@naver.com>
2022-04-14 15:13:01 +0900

    220414_url 정리

--- Instrumentapp/urls.py
+++ Instrumentapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+app_name = "Instrumentapp"
 
 urlpatterns = [
     path("<str:category>/<str:instrument_name>", views.InstrumentListView.as_view(), name="instrument"),
