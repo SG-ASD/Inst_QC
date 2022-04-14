de20cae446e3bb93d894a2f011915d585d68d622
tlsgn511 <shlee2@seegene.com>
2022-04-15 08:45:16 +0900

    Commit Urls 연결
    Commit Urls 연결

--- Userapp/urls.py
+++ Userapp/urls.py
@@ -8,6 +8,7 @@ app_name = "Userapp"
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='Userapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+
     path('create/', UserCreateView.as_view(), name='create'),
     path('detail/<int:pk>', UserDetailView.as_view(), name='detail'),
     path('update/<int:pk>', UserUpdateView.as_view(), name='update'),
