50d1da9ffc12bf517d2e7daf9e8560be51477a74
ch8638 <ch8638@naver.com>
2022-04-15 15:07:33 +0900

    220415_외래키 데이터 생성

--- Instrumentapp/models.py
+++ Instrumentapp/models.py
@@ -4,8 +4,5 @@ from django.db import models
 
 
 class Instrument(models.Model):
-    SN = models.CharField(max_length=30, unique=True)  # 장비 SN
+    SN = models.CharField(primary_key=True, max_length=30, unique=True)  # 장비 SN
     Name = models.CharField(max_length=60)  # 장비명
-
-    def __str__(self):
-        return self.SN
--- Instrumentapp/views.py
+++ Instrumentapp/views.py
@@ -15,6 +15,7 @@ from Mainapp.models import Category
 from Inspectionapp.models import Inspection
 from .forms import InstrumentForm
 
+
 # Create your views here.
 
 
@@ -23,6 +24,7 @@ class InstrumentListView(ListView):
     template_name = "Instrumentapp/instrument.html"
     context_object_name = "instrument_list"
     pk_url_kwarg = "category"
+
     # pk_url_kwarg = "instrument_name"
 
     def post(self, request, *args, **kwargs):
@@ -37,11 +39,21 @@ class InstrumentListView(ListView):
 
             context = {"s2": inst_SN, "s1": inst_name}
 
-            new_inst = Instrument(SN=inst_SN, Name=inst_name)
-            new_inspection = Inspection(Instrument_SN=inst_SN, Name=inst_name, Status="검사대기")
+            # new_inst = Instrument(SN=inst_SN, Name=inst_name)
+            # new_inst.save()
+            # new_inspection = Inspection(Instrument_SN=new_inst.SN, Name=inst_name, Status="검사대기")
+            # new_inspection.save()
 
-            new_inst.save()
-            new_inspection.save()
+            new_instrument = Instrument.objects.create(
+                SN=inst_SN,
+                Name=inst_name
+            )
+
+            new_inspection = Inspection.objects.create(
+                Instrument_SN=new_instrument,
+                Name=inst_name,
+                Status="검사대기"
+            )
 
         print(f"context : {context}")
         print(f"category : {category}")
@@ -131,7 +143,6 @@ class InstrumentListView(ListView):
 
         return instrument_list
 
-
 # class InstrumentCreateView(CreateView):
 #     model = Instrument
 #     form_class = InstrumentForm
@@ -139,27 +150,27 @@ class InstrumentListView(ListView):
 #     context_object_name = "instrument_list"
 #     pk_url_kwarg = "category"
 
-    # @transaction.atomic
-    # def get_queryset(self):  # ListView가 전달하는 object를 바꾸고 싶으면 get_queryset을 오버라이드 한다.(object 여러개)
-    #     category = self.kwargs.get("category")
-    #     temp1 = Category.objects.filter(Category=category).values('Subcategory')
-    #     # print(Category.objects.filter(Category=main_category).values('Subcategory').query)
-    #     print(temp1)
-    #     return temp1
-    # def index(self):
-    #     return render(self.request, "Instrumentapp/add_instrument.html")
+# @transaction.atomic
+# def get_queryset(self):  # ListView가 전달하는 object를 바꾸고 싶으면 get_queryset을 오버라이드 한다.(object 여러개)
+#     category = self.kwargs.get("category")
+#     temp1 = Category.objects.filter(Category=category).values('Subcategory')
+#     # print(Category.objects.filter(Category=main_category).values('Subcategory').query)
+#     print(temp1)
+#     return temp1
+# def index(self):
+#     return render(self.request, "Instrumentapp/add_instrument.html")
 
-    # def form_valid(self, form):  # 입력받은 데이터가 유효할 때 데이터로 채워진 모델 오브젝트를 만들고 오브젝트를 저장하는 메소드
-    #     print("post!!!")
-    #     print(self.request.POST.get("s1"))
-    #     # view에서 현재 user에 접근할 때는 request.user를 사용
-    #     # 저장될 폼에 새로운 속성을 추가하려면 form.instance에 속성을 추가하고 꼭 CreateView의 form_vaild 메소드를 호출해야 한다. 호출하지 않으면 폼이 저장되지 않는다.
-    #     form.instance.author = self.request.user  # 함수형에서는 request가 view 파라미터로 전달. 클래스형 뷰에서는 self.request로 접근해야 한다.
-    #     print(f"form.instance : {form.instance}")
-    #     return super().form_valid(form)  # super는 ReviewCreateView의 상위 클래스, 즉 CreateView를 의미
-    #
-    # def get_success_url(self):
-    #     category = self.kwargs.get("category")
-    #     instrument_name = self.kwargs.get("instrument_name")
-    #     # self.object는 현재 제네릭 뷰가 다루고 있는 object라고 생각하면 된다.
-    #     return reverse("instrument", kwargs={"category": category, "instrument_name": instrument_name})  # review-detail에 id를 파라미터로 넘긴다.
+# def form_valid(self, form):  # 입력받은 데이터가 유효할 때 데이터로 채워진 모델 오브젝트를 만들고 오브젝트를 저장하는 메소드
+#     print("post!!!")
+#     print(self.request.POST.get("s1"))
+#     # view에서 현재 user에 접근할 때는 request.user를 사용
+#     # 저장될 폼에 새로운 속성을 추가하려면 form.instance에 속성을 추가하고 꼭 CreateView의 form_vaild 메소드를 호출해야 한다. 호출하지 않으면 폼이 저장되지 않는다.
+#     form.instance.author = self.request.user  # 함수형에서는 request가 view 파라미터로 전달. 클래스형 뷰에서는 self.request로 접근해야 한다.
+#     print(f"form.instance : {form.instance}")
+#     return super().form_valid(form)  # super는 ReviewCreateView의 상위 클래스, 즉 CreateView를 의미
+#
+# def get_success_url(self):
+#     category = self.kwargs.get("category")
+#     instrument_name = self.kwargs.get("instrument_name")
+#     # self.object는 현재 제네릭 뷰가 다루고 있는 object라고 생각하면 된다.
+#     return reverse("instrument", kwargs={"category": category, "instrument_name": instrument_name})  # review-detail에 id를 파라미터로 넘긴다.
