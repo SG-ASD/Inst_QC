51c254eca1bf2ab3a67e54254664c9ff1e9341dc
tlsgn511 <shlee2@seegene.com>
2022-04-18 20:58:11 +0900

    Revert "220418_inspection 중간 테스트"
    This reverts commit ec55ea5767c68aadc5cb24650167f1f8a3aca801.

--- Inspectionapp/forms.py
+++ Inspectionapp/forms.py
@@ -1,13 +1,13 @@
 from django.forms import ModelForm
 
-from .models import Inspection
+from Inspectionapp.models import Inspection
 
 
 class InspectionUpdateForm(ModelForm):
     class Meta:
         model = Inspection
-        fields = ['Instrument_SN', 'Inspector', 'Computer_SN', 'Revision', 'Start_Date', 'Completed_Date', 'SW_Version',
-                  'FV2_Version', 'FW_PipetteCh', 'FW_Xdrive', 'FW_Master']
+        fields = ['Instrument_SN', 'Inspector', 'Computer_SN', 'Revision', 'Start_Date', 'Completed_Date','SW_Version',
+                    'FV2_Version', 'FW_PipetteCh', 'FW_Xdrive', 'FW_Master']
 
 
 
