ec55ea5767c68aadc5cb24650167f1f8a3aca801
ch8638 <ch8638@naver.com>
2022-04-18 20:20:37 +0900

    220418_inspection 중간 테스트

--- Inspectionapp/views.py
+++ Inspectionapp/views.py
@@ -5,20 +5,37 @@ from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
-from Inspectionapp.decorators import Inspection_ownership_required
+from .decorators import Inspection_ownership_required
 from Userapp.decorators import User_ownership_required
-from Inspectionapp.models import Inspection
-from Inspectionapp.forms import InspectionUpdateForm
+from .models import Inspection
+from .forms import InspectionUpdateForm
 
 # @method_decorator(Inspection_ownership_required, 'get')
 # @method_decorator(Inspection_ownership_required, 'post')
+
+
 class InspectionUpdateView(UpdateView):
     model = Inspection
-    context_object_name = 'target_Inspection'
     form_class = InspectionUpdateForm
-    success_url = reverse_lazy('Mainapp:index')
-
     template_name = 'Inspectionapp/update.html'
+    context_object_name = 'target_Inspection'
+    pk_url_kwarg = "SN"
+    # success_url = reverse_lazy('Mainapp:index')
+
+    def get_queryset(self):
+        SN = self.kwargs.get("SN")
+        print(f"SN : {SN}")
+
+        temp = Inspection.objects.get(Instrument_SN=SN)
+        # print(Inspection.objects.get(Instrument_SN=SN))
+        print(f"SN_id : {temp.Instrument_SN_id}")
+        print(f"type : {type(temp.Instrument_SN_id)}")
+
+        instrument_info = Inspection.objects.filter(Instrument_SN=temp.Instrument_SN_id).values()
+        print(instrument_info)
+
+        return instrument_info
+
 
     # def get_success_url(self):
     #     return reverse('Instrumentapp:instrument', kwargs={'pk': self.object.pk})
