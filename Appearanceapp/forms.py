0454787c1b87cab2ad2571ed5a2eb775c6d4e0a7
ch8638 <ch8638@naver.com>
2022-04-22 08:39:10 +0900

    220422_Appearance

--- Appearanceapp/forms.py
+++ Appearanceapp/forms.py
@@ -1,13 +1,14 @@
 from django.forms import ModelForm
 
-
 from Inspectionapp.models import Inspection
+
 
 class AppearanceUpdateForm(ModelForm):
     class Meta:
         model = Inspection
         fields = ['Appearance_Shock_Watch', 'Appearance_Binding', 'Appearance_Labels', 'Appearance_Packaging_Box',
-        'Appearance_Wooden_Pallet','Appearance_Transport_Jig',
-        'Appearance_Shock_Watch_Image', 'Appearance_Binding_Image', 'Appearance_Labels_Image', 'Appearance_Packaging_Box_Image',
-        'Appearance_Wooden_Pallet_Image', 'Appearance_Transport_Jig_Image'
-        ]
+                  'Appearance_Wooden_Pallet', 'Appearance_Transport_Jig',
+                  'Appearance_Shock_Watch_Image', 'Appearance_Binding_Image', 'Appearance_Labels_Image',
+                  'Appearance_Packaging_Box_Image',
+                  'Appearance_Wooden_Pallet_Image', 'Appearance_Transport_Jig_Image', 'Appearance_Video',
+                  ]
--- Appearanceapp/views.py
+++ Appearanceapp/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db import transaction
 from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
@@ -6,11 +7,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import UpdateView
 
-from Inspectionapp.models import Inspection
+from Inspectionapp.models import Inspection, Inspection_Category
 from Userapp.decorators import User_ownership_required
 from Appearanceapp.forms import AppearanceUpdateForm
 
@@ -20,18 +21,31 @@ has_ownership = [User_ownership_required]
 @method_decorator(login_required, 'post')
 class AppearanceUpdateView(UpdateView):
     model = Inspection
-    context_object_name = 'target_Appearance'
     form_class = AppearanceUpdateForm
-    success_url = reverse_lazy('Mainapp:index')
-
     template_name = 'Appearanceapp/update.html'
+    context_object_name = 'target_Appearance'
 
     def get_object(self):
         object = get_object_or_404(Inspection, Instrument_SN=self.kwargs['Instrument_SN'])
         return object
 
-    def form_valid(self, form):
-        temp_Appearance = form.save(commit=False)
-        temp_Appearance.user = self.request.user
-        temp_Appearance.save()
-        return super().form_valid(form)
+    @transaction.atomic
+    def get_context_data(self, **kwargs):
+        context = super(AppearanceUpdateView, self).get_context_data(**kwargs)
+        # subcategory = self.kwargs.get("category")
+        context["inspection_category"] = Inspection_Category.objects.distinct().values_list('Category', flat=True)
+        context["inspection_subcategory"] = Inspection_Category.objects.distinct().values_list('Subcategory', flat=True)
+        print(f"context : {context}")
+
+        return context
+
+    def get_success_url(self):
+        # return reverse('Instrumentapp:instrument', kwargs={'pk': self.object.pk})
+        return reverse("Appearanceapp:update", kwargs={"Instrument_SN": self.object})
+
+
+    # def form_valid(self, form):
+    #     temp_Appearance = form.save(commit=False)
+    #     temp_Appearance.user = self.request.user
+    #     temp_Appearance.save()
+    #     return super().form_valid(form)
