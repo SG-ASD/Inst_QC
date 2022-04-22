a7f9ecc052636cd80f24e5c67e39db1cc8ff0383
ch8638 <ch8638@naver.com>
2022-04-22 16:19:25 +0900

    220422_all pass

--- static/js/Appearanceapp.js
+++ static/js/Appearanceapp.js
@@ -16,28 +16,47 @@ function alert_save(){
 function select_category(obj) {
     if (obj.value == "Appearance Inspection") {
         document.getElementById("inspection_subcategory").innerHTML = `
-            <option value="Packaging Condition">Packaging Condition</option>
-            <option value="Appearance of instrument">Appearance of instrument</option>
-            <option value="Instrument Accessories Kits">Instrument Accessories Kits</option>`;
+            <option value="Packaging Condition">1) Packaging Condition</option>
+            <option value="Appearance of instrument">2) Appearance of instrument</option>
+            <option value="Instrument Accessories Kits">3) Instrument Accessories Kits</option>`;
     }
     else if (obj.value == "Electrical Test") {
         document.getElementById("inspection_subcategory").innerHTML = `
-            <option value="Electrical Test">Electrical Test</option>`;
+            <option value="Electrical Test">1) Electrical Test</option>`;
     }
     else if (obj.value == "Hardware Adjustment") {
         document.getElementById("inspection_subcategory").innerHTML = `
-            <option value="Hardware Adjustment">Hardware Adjustment</option>`;
+            <option value="Hardware Adjustment">1) Hardware Adjustment</option>`;
     }
     else if (obj.value == "Performance Verification") {
         document.getElementById("inspection_subcategory").innerHTML = `
-            <option value="Performance Verification">Performance Verification</option>`;
+            <option value="Performance Verification">1) Performance Verification</option>`;
     }
     else if (obj.value == "Attachment") {
         document.getElementById("inspection_subcategory").innerHTML = `
-            <option value="Attachment">Attachment</option>`;
+            <option value="Attachment">1) Attachment</option>`;
     }
     else if (obj.value == "Accessories Kits") {
         document.getElementById("inspection_subcategory").innerHTML = `
-            <option value="Accessories Kits">Accessories Kits</option>`;
+            <option value="Accessories Kits">1) Accessories Kits</option>`;
     }
+}
+
+/**
+ * All Pass 체크 시 하위 체크박스에 체크 표시
+ */
+function onchange_check() {
+    const Appearance_Shock_Watch = document.getElementById('Appearance_Shock_Watch1');
+    const Appearance_Binding = document.getElementById('Appearance_Binding2');
+    const Appearance_Labels = document.getElementById('Appearance_Labels2');
+    const Appearance_Packaging_Box = document.getElementById('Appearance_Packaging_Box1');
+    const Appearance_Wooden_Pallet = document.getElementById('Appearance_Wooden_Pallet1');
+    const Appearance_Transport_Jig = document.getElementById('Appearance_Transport_Jig2');
+
+    Appearance_Shock_Watch.click()
+    Appearance_Binding.click()
+    Appearance_Labels.click()
+    Appearance_Packaging_Box.click()
+    Appearance_Wooden_Pallet.click()
+    Appearance_Transport_Jig.click()
 }
