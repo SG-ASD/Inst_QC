278da448a2165acb6a97ce1474754580744e55a3
shinhoo <shlee2@seegene.com>
2022-05-11 16:03:08 +0900

    Hardware 기능업데이트

--- Hardwareapp/models.py
+++ Hardwareapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+class Calibration(models.Model):
+    Managemant_Num = models.CharField(primary_key=True, max_length=30, unique=True)  # 관리번호
+    Instrument = models.CharField(max_length=60)  # 장비명
+    Description = models.CharField(max_length=60)  # 설비 및 계측기
+    State = models.CharField(max_length=60)  # 가능 
+    Type = models.CharField(max_length=60)  # 유형
+    Calibration_Date = models.DateField(auto_now=True) # 교정일자
+    Validation_Date = models.DateField(auto_now=True) # 유효기간
+
+    def __str__(self):
+        return self.Calibration
+
+
+
+
+
--- Hardwareapp/views.py
+++ Hardwareapp/views.py
@@ -1,5 +1,8 @@
+from datetime import date
+
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
+from django.db.models.functions import datetime
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -11,6 +14,7 @@ from Inspectionapp.models import Inspection, Inspection_Category
 from Userapp.decorators import User_ownership_required
 from .forms import HardwareUpdateForm_first
 from .forms import HardwareUpdateForm_second
+from .models import Calibration
 
 has_ownership = [User_ownership_required]
 
@@ -22,6 +26,7 @@ class HardwareUpdateView_first(UpdateView):
     form_class = HardwareUpdateForm_first
     template_name = 'Hardwareapp/hardware_first.html'
     context_object_name = 'target_Hardware'
+    startdate = date(2012, 12, 11)
 
     def get_object(self):
         object = get_object_or_404(Inspection, Instrument_SN=self.kwargs['Instrument_SN'])
@@ -30,8 +35,12 @@ class HardwareUpdateView_first(UpdateView):
     @transaction.atomic
     def get_context_data(self, **kwargs):
         context = super(HardwareUpdateView_first, self).get_context_data(**kwargs)
+        Instrument_Nm = context['object'].Name
+
         context["inspection_category"] = Inspection_Category.objects.distinct().values_list('Category', flat=True)
         context["inspection_subcategory"] = Inspection_Category.objects.filter(Category="Electrical Test").values_list('Subcategory', flat=True)
+        context["inspection_calibration"] = Calibration.objects.filter(Instrument=Instrument_Nm).all()
+        context["inspection_Barcode"] = Calibration.objects.filter(Description__contains='Barcode').all()
         return context
 
     def get_success_url(self):
