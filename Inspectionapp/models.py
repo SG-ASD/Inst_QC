af21db300ce346d39432d7198d6975ba9ecfcd16
tlsgn8483 <100942565+tlsgn511@users.noreply.github.com>
2022-07-05 11:01:22 +0900

    배포 환경 테스트
    배포 환경 테스트

--- Inspectionapp/models.py
+++ Inspectionapp/models.py
@@ -31,26 +31,26 @@ class Inspection(models.Model):
 
     # Inspection
     Instrument_SN = models.OneToOneField(Instrument, on_delete=models.CASCADE, db_column='Instrument_SN', related_name='Inspection', primary_key=True, unique=True)  # 장비 SN
-    Name = models.CharField(max_length=60, blank=True)  # 장비명
-    Inspector = models.CharField(max_length=20, blank=True)  # 검사자
+    Name = models.TextField(max_length=60, blank=True)  # 장비명
+    Inspector = models.TextField(max_length=20, blank=True)  # 검사자
     Start_Date = models.DateField(null=True, blank=True)  # 검사시작일
     CompleteDt = models.DateField(null=True, blank=True)  # 검사완료일
-    Status = models.CharField(max_length=20, blank=True)  # 검사 상태
-    Revision = models.CharField(max_length=50, blank=True)  # 검사성적서 Revision
-    SW_Version = models.CharField(max_length=50, blank=True)  # SW 버전
-    FV2_Version = models.CharField(max_length=50, blank=True)  # FV2 버전
-    FW_PipetteCh = models.CharField(max_length=50, blank=True)  # PipetteCh 버전
-    FW_Xdrive = models.CharField(max_length=50, blank=True)  # X-drive 버전
-    FW_Master = models.CharField(max_length=50, blank=True)  # Master 버전
-    Computer_SN = models.CharField(max_length=50, blank=True)  # 컴퓨터 SN
+    Status = models.TextField(max_length=20, blank=True)  # 검사 상태
+    Revision = models.TextField(max_length=50, blank=True)  # 검사성적서 Revision
+    SW_Version = models.TextField(max_length=50, blank=True)  # SW 버전
+    FV2_Version = models.TextField(max_length=50, blank=True)  # FV2 버전
+    FW_PipetteCh = models.TextField(max_length=50, blank=True)  # PipetteCh 버전
+    FW_Xdrive = models.TextField(max_length=50, blank=True)  # X-drive 버전
+    FW_Master = models.TextField(max_length=50, blank=True)  # Master 버전
+    Computer_SN = models.TextField(max_length=50, blank=True)  # 컴퓨터 SN
 
     # 1.Appearance
-    Appearance_Shock_Watch = models.CharField(max_length=20, blank=True)
-    Appearance_Binding = models.CharField(max_length=20, blank=True)
-    Appearance_Labels = models.CharField(max_length=20, blank=True)
-    Appearance_Packaging_Box = models.CharField(max_length=20, blank=True)
-    Appearance_Wooden_Pallet = models.CharField(max_length=20, blank=True)
-    Appearance_Transport_Jig = models.CharField(max_length=20, blank=True)
+    Appearance_Shock_Watch = models.TextField(max_length=20, blank=True)
+    Appearance_Binding = models.TextField(max_length=20, blank=True)
+    Appearance_Labels = models.TextField(max_length=20, blank=True)
+    Appearance_Packaging_Box = models.TextField(max_length=20, blank=True)
+    Appearance_Wooden_Pallet = models.TextField(max_length=20, blank=True)
+    Appearance_Transport_Jig = models.TextField(max_length=20, blank=True)
 
     Appearance_Shock_Watch_Image = models.ImageField(upload_to=test_path, blank=True)
     Appearance_Binding_Image = models.ImageField(upload_to=Appearance_image_path, blank=True)
@@ -60,28 +60,28 @@ class Inspection(models.Model):
     Appearance_Transport_Jig_Image = models.ImageField(upload_to=Appearance_image_path, blank=True)
     Appearance_Video = models.FileField(upload_to=Appearance_image_path, blank=True)
 
-    Appearance_Front = models.CharField(max_length=20, blank=True)
-    Appearance_Top = models.CharField(max_length=20, blank=True)
-    Appearance_Right = models.CharField(max_length=20, blank=True)
-    Appearance_Left = models.CharField(max_length=20, blank=True)
-    Appearance_Back = models.CharField(max_length=20, blank=True)
-    Appearance_Acc_Damage = models.CharField(max_length=20, blank=True)
-    Appearance_Acc_Missing = models.CharField(max_length=20, blank=True)
+    Appearance_Front = models.TextField(max_length=20, blank=True)
+    Appearance_Top = models.TextField(max_length=20, blank=True)
+    Appearance_Right = models.TextField(max_length=20, blank=True)
+    Appearance_Left = models.TextField(max_length=20, blank=True)
+    Appearance_Back = models.TextField(max_length=20, blank=True)
+    Appearance_Acc_Damage = models.TextField(max_length=20, blank=True)
+    Appearance_Acc_Missing = models.TextField(max_length=20, blank=True)
 
     # 2.ElectricalTest
-    ElectricalTest_HiPotential = models.CharField(max_length=20, blank=True)
-    ElectricalTest_GroundResistance = models.CharField(max_length=20, blank=True)
-    ElectricalTest_PowerSWFunction = models.CharField(max_length=20, blank=True)
-    ElectricalTest_PowerLED = models.CharField(max_length=20, blank=True)
+    ElectricalTest_HiPotential = models.TextField(max_length=20, blank=True)
+    ElectricalTest_GroundResistance = models.TextField(max_length=20, blank=True)
+    ElectricalTest_PowerSWFunction = models.TextField(max_length=20, blank=True)
+    ElectricalTest_PowerLED = models.TextField(max_length=20, blank=True)
 
     # 3.HardWare
-    HardWare_CalibrationTool = models.CharField(max_length=20, blank=True)
-    HardWare_BarcodeCarrier = models.CharField(max_length=20, blank=True)
+    HardWare_CalibrationTool = models.TextField(max_length=20, blank=True)
+    HardWare_BarcodeCarrier = models.TextField(max_length=20, blank=True)
     HardWare_XArm_left = models.IntegerField(null=True, blank=True)
     HardWare_XArm_right = models.IntegerField(null=True, blank=True)
     HardWare_XArm_Diff = models.IntegerField(null=True, blank=True)
-    HardWare_Autoload = models.CharField(max_length=20, blank=True)
-    HardWare_Noise = models.CharField(max_length=20, blank=True)
+    HardWare_Autoload = models.TextField(max_length=20, blank=True)
+    HardWare_Noise = models.TextField(max_length=20, blank=True)
     HardWare_Xdev1 = models.IntegerField(null=True, blank=True)
     HardWare_Xdev2 = models.IntegerField(null=True, blank=True)
     HardWare_Xdev3 = models.IntegerField(null=True, blank=True)
@@ -106,15 +106,15 @@ class Inspection(models.Model):
     HardWare_Ytilt6 = models.IntegerField(null=True, blank=True)
     HardWare_Ytilt7 = models.IntegerField(null=True, blank=True)
     HardWare_Ytilt8 = models.IntegerField(null=True, blank=True)
-    HardWare_SN1 = models.CharField(max_length=20, blank=True)
-    HardWare_SN2 = models.CharField(max_length=20, blank=True)
-    HardWare_SN3 = models.CharField(max_length=20, blank=True)
-    HardWare_SN4 = models.CharField(max_length=20, blank=True)
-    HardWare_SN5 = models.CharField(max_length=20, blank=True)
-    HardWare_SN6 = models.CharField(max_length=20, blank=True)
-    HardWare_SN7 = models.CharField(max_length=20, blank=True)
-    HardWare_SN8 = models.CharField(max_length=20, blank=True)
-    HardWare_Adjust = models.CharField(max_length=20, blank=True)
+    HardWare_SN1 = models.TextField(max_length=20, blank=True)
+    HardWare_SN2 = models.TextField(max_length=20, blank=True)
+    HardWare_SN3 = models.TextField(max_length=20, blank=True)
+    HardWare_SN4 = models.TextField(max_length=20, blank=True)
+    HardWare_SN5 = models.TextField(max_length=20, blank=True)
+    HardWare_SN6 = models.TextField(max_length=20, blank=True)
+    HardWare_SN7 = models.TextField(max_length=20, blank=True)
+    HardWare_SN8 = models.TextField(max_length=20, blank=True)
+    HardWare_Adjust = models.TextField(max_length=20, blank=True)
     HardWare_PIP_X1 = models.IntegerField(null=True, blank=True, default=0)
     HardWare_PIP_X2 = models.IntegerField(null=True, blank=True, default=0)
     HardWare_PIP_X3 = models.IntegerField(null=True, blank=True, default=0)
@@ -141,61 +141,61 @@ class Inspection(models.Model):
     HardWare_PIP_Z8 = models.IntegerField(null=True, blank=True, default=0)
 
     # 4.Perfomance
-    Perfomance_CoverSafety = models.CharField(max_length=20, blank=True)
-    Perfomance_Barcode = models.CharField(max_length=20, blank=True)
-    Perfomance_XYZpositioning = models.CharField(max_length=20, blank=True)
-    Perfomance_HHS_SN = models.CharField(max_length=20, blank=True)
-    Perfomance_HHS_temperature = models.CharField(max_length=20, blank=True)
-    Perfomance_HHS_RPM = models.CharField(max_length=20, blank=True)
-    Perfomance_Loading_TipCarrier = models.CharField(max_length=20, blank=True)
-    Perfomance_Loading_TubeCarrier = models.CharField(max_length=20, blank=True)
-    Perfomance_Loading_mtpCarrier = models.CharField(max_length=20, blank=True)
-    Perfomance_Loading_4mfxCarrier = models.CharField(max_length=20, blank=True)
+    Perfomance_CoverSafety = models.TextField(max_length=20, blank=True)
+    Perfomance_Barcode = models.TextField(max_length=20, blank=True)
+    Perfomance_XYZpositioning = models.TextField(max_length=20, blank=True)
+    Perfomance_HHS_SN = models.TextField(max_length=20, blank=True)
+    Perfomance_HHS_temperature = models.TextField(max_length=20, blank=True)
+    Perfomance_HHS_RPM = models.TextField(max_length=20, blank=True)
+    Perfomance_Loading_TipCarrier = models.TextField(max_length=20, blank=True)
+    Perfomance_Loading_TubeCarrier = models.TextField(max_length=20, blank=True)
+    Perfomance_Loading_mtpCarrier = models.TextField(max_length=20, blank=True)
+    Perfomance_Loading_4mfxCarrier = models.TextField(max_length=20, blank=True)
 
-    Perfomance_VFV_Accuracy300ul1 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Accuracy300ul2 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Accuracy300ul3 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Accuracy300ul4 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Accuracy300ul5 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Accuracy300ul6 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Accuracy300ul7 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Accuracy300ul8 = models.CharField(max_length=20, blank=True)
+    Perfomance_VFV_Accuracy300ul1 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Accuracy300ul2 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Accuracy300ul3 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Accuracy300ul4 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Accuracy300ul5 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Accuracy300ul6 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Accuracy300ul7 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Accuracy300ul8 = models.TextField(max_length=20, blank=True)
 
-    Perfomance_VFV_Precision300ul1 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Precision300ul2 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Precision300ul3 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Precision300ul4 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Precision300ul5 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Precision300ul6 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Precision300ul7 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Precision300ul8 = models.CharField(max_length=20, blank=True)
+    Perfomance_VFV_Precision300ul1 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Precision300ul2 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Precision300ul3 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Precision300ul4 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Precision300ul5 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Precision300ul6 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Precision300ul7 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Precision300ul8 = models.TextField(max_length=20, blank=True)
 
-    Perfomance_VFV_Accuracy1000ul1 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Accuracy1000ul2 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Accuracy1000ul3 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Accuracy1000ul4 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Accuracy1000ul5 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Accuracy1000ul6 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Accuracy1000ul7 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Accuracy1000ul8 = models.CharField(max_length=20, blank=True)
+    Perfomance_VFV_Accuracy1000ul1 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Accuracy1000ul2 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Accuracy1000ul3 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Accuracy1000ul4 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Accuracy1000ul5 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Accuracy1000ul6 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Accuracy1000ul7 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Accuracy1000ul8 = models.TextField(max_length=20, blank=True)
 
-    Perfomance_VFV_Precision1000ul1 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Precision1000ul2 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Precision1000ul3 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Precision1000ul4 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Precision1000ul5 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Precision1000ul6 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Precision1000ul7 = models.CharField(max_length=20, blank=True)
-    Perfomance_VFV_Precision1000ul8 = models.CharField(max_length=20, blank=True)
+    Perfomance_VFV_Precision1000ul1 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Precision1000ul2 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Precision1000ul3 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Precision1000ul4 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Precision1000ul5 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Precision1000ul6 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Precision1000ul7 = models.TextField(max_length=20, blank=True)
+    Perfomance_VFV_Precision1000ul8 = models.TextField(max_length=20, blank=True)
 
     # 5.Attachment
-    Attachment_CoverSafety_Report = models.CharField(max_length=20, null=True, blank=True)
-    Attachment_Barcode_Report = models.CharField(max_length=20, null=True, blank=True)
-    Attachment_Position_Report = models.CharField(max_length=20, null=True, blank=True)
-    Attachment_Declaration_HHS = models.CharField(max_length=20, null=True, blank=True)
-    Attachment_Declaration = models.CharField(max_length=20, null=True, blank=True)
-    Attachment_Measurement_Protocol = models.CharField(max_length=20, null=True, blank=True)
-    Attachment_ElectricalSafety_Report = models.CharField(max_length=20, null=True, blank=True)
+    Attachment_CoverSafety_Report = models.TextField(max_length=20, null=True, blank=True)
+    Attachment_Barcode_Report = models.TextField(max_length=20, null=True, blank=True)
+    Attachment_Position_Report = models.TextField(max_length=20, null=True, blank=True)
+    Attachment_Declaration_HHS = models.TextField(max_length=20, null=True, blank=True)
+    Attachment_Declaration = models.TextField(max_length=20, null=True, blank=True)
+    Attachment_Measurement_Protocol = models.TextField(max_length=20, null=True, blank=True)
+    Attachment_ElectricalSafety_Report = models.TextField(max_length=20, null=True, blank=True)
     Attachment_CoverSafety_Report_File = models.FileField(upload_to=Attachment_image_path, null=True, blank=True)
     Attachment_Barcode_Report_File = models.FileField(upload_to=Attachment_image_path, null=True, blank=True)
     Attachment_Position_Report_File = models.FileField(upload_to=Attachment_image_path, null=True, blank=True)
