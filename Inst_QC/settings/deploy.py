1ee0fc4cb156fcf7948e9ac7a47afd9e5197b08f
tlsgn8483 <100942565+tlsgn511@users.noreply.github.com>
2022-07-06 09:12:34 +0900

    배포 테스트
    배포 테스트

--- Inst_QC/settings/deploy.py
+++ Inst_QC/settings/deploy.py
@@ -13,7 +13,11 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'postgresql',
+        'PORT': '5432',
     }
 }
