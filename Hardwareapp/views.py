26a78dd65961fb7dddb8fcb6cb5b846d9d317676
tlsgn8483 <100942565+tlsgn511@users.noreply.github.com>
2022-07-13 15:53:21 +0900

    임시 저장
    임시 저장

--- Hardwareapp/views.py
+++ Hardwareapp/views.py
@@ -3,7 +3,8 @@ from datetime import date, timedelta
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
 from django.db.models.functions import datetime
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect, resolve_url
 
 # Create your views here.
 from django.urls import reverse_lazy, reverse
@@ -36,12 +37,24 @@ class HardwareUpdateView_first(UpdateView):
     def get_context_data(self, **kwargs):
         context = super(HardwareUpdateView_first, self).get_context_data(**kwargs)
         Instrument_Nm = context['object'].Name
+
+
+
         context["inspection_category"] = Inspection_Category.objects.distinct().values_list('Category', flat=True)
-        context["inspection_calibration"] = Calibration.objects.filter(Instrument=Instrument_Nm).filter(Description__contains='Channel Calibration Tool').\
-            filter(CalibrationDt__lte=date.today(), ValidationDt__gte=date.today())
-        context["inspection_Barcode"] = Calibration.objects.filter(Instrument=Instrument_Nm).filter(Description__contains='Barcode Carrier').\
-            filter(CalibrationDt__lte=date.today(), ValidationDt__gte=date.today())
+        context["inspection_calibration"] = Calibration.objects.filter(Instrument=Instrument_Nm).filter(Equipment_Name__contains='Channel Calibration Tool').\
+            filter(CAL_Date__lte=date.today(), Expiration_Date__gte=date.today())
+        context["inspection_Barcode"] = Calibration.objects.filter(Instrument=Instrument_Nm).filter(Equipment_Name__contains='Barcode Carrier').\
+            filter(CAL_Date__lte=date.today(), Expiration_Date__gte=date.today())
         return context
+
+    def get(request):
+        Left = request.GET.get('formFile', '')
+        Right = request.GET.get('formFile', '')
+        P1_dev = request.GET.get('formFile', '')
+        q = request.GET.get('formFile', '')
+        q = request.GET.get('formFile', '')
+        q = request.GET.get('formFile', '')
+        q = request.GET.get('formFile', '')
 
     def get_success_url(self):
         return reverse("Hardwareapp:update_Hardware2", kwargs={"Instrument_SN": self.object})
@@ -66,3 +79,13 @@ class HardwareUpdateView_second(UpdateView):
 
     def get_success_url(self):
         return reverse("Performanceapp:update_Performance1", kwargs={"Instrument_SN": self.object})
+
+@login_required
+def post_detail(request, Instrument_SN):
+    qs = Inspection.objects.all()
+    q = request.GET.get('formFile', '')
+    if q:
+        qs = qs.filter(message__icontains=q)
+        # instagram/templetes/instagram/post_list.html
+    return redirect("Hardwareapp:update_Hardware1", Instrument_SN)
+    # return resolve_url("Hardwareapp:update_Hardware1", Instrument_SN=Instrument_SN)
