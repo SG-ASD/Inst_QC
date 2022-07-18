540ce9222b635a2324d2d11ff2ff1734c656b098
ch8638 <ch8638@naver.com>
2022-07-18 13:34:45 +0900

    local 환경 세팅

--- Inst_QC/settings/deploy.py
+++ Inst_QC/settings/deploy.py
@@ -8,9 +8,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
