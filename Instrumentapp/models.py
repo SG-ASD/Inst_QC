b0ba0aa90a9f798c81e3ee086dc7fdb118ff613a
tlsgn8483 <100942565+tlsgn511@users.noreply.github.com>
2022-07-19 14:24:31 +0900

    Revision 관리자화면 생성
    Revision 관리자화면 생성

--- Instrumentapp/models.py
+++ Instrumentapp/models.py
@@ -9,3 +9,37 @@ class Instrument(models.Model):
 
     def __str__(self):
         return self.SN
+
+class Revision(models.Model):
+    Instrument = models.CharField(
+        # 튜플 안에 튜플
+        choices=(
+            # DB에 저장될 값과 사용자에게 보여줄 값
+            ('공용', '공용'),
+            ('Seegene STARlet', 'Seegene STARlet'),
+            ('Hamilton STARlet', 'Hamilton STARlet'),
+            ('Seegene Nimbus', 'Seegene Nimbus'),
+            ('CFX 96Dx', 'CFX 96Dx'),
+            ('Seeprep32', 'Seeprep32'),
+            ('SGRT', 'SGRT'),
+            ('AIOS', 'AIOS'),
+            ('VCMS', 'VCMS'),
+            ('모바일스테이션', '모바일스테이션'),
+            ('기타', '기타'),
+        ),
+        default='Seegene STARlet', max_length=32, verbose_name='장비이름'
+    )  # 장비명
+    Report_Category = models.CharField(
+        choices=(
+        # DB에 저장될 값과 사용자에게 보여줄 값
+        ('Inspection Report', '수입검사 성적서'),
+        ('Finished Product Report', '완제품검사 성적서'),
+        ('Finished Product Report Rev', '완제품 지침서'),
+        ('Finished Label', '완제품 라벨'),
+    ),
+       max_length=40, verbose_name='성적서 종류'
+    )
+    Rev_Num = models.CharField(max_length=50, ) # 관리번호
+    Rev = models.IntegerField(null=False) # Revision
+    Start_Dt = models.DateField(auto_now=True, verbose_name='사용 시작일')
+    End_Dt = models.DateField(auto_now=True, verbose_name='사용 종료일')
