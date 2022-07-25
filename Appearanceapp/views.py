7ad60c7c7ae413e96b4f5db1c473d2457780736c
ch8638 <ch8638@naver.com>
2022-07-25 09:32:00 +0900

    공유폴더 연결

--- Appearanceapp/views.py
+++ Appearanceapp/views.py
@@ -58,12 +58,9 @@ class AppearanceUpdateView(UpdateView):
                 form_instance.Appearance_Transport_Jig = request.POST.get('Appearance_Transport_Jig')
 
                 # 파일 upload
-                NAS_path = r"\\10.10.102.76\장비품질관리팀\품질관리_장비inspection\01 검사 성적서 관리\2022 검사 성적서\QC SW 테스트"  # NAS 폴더 경로
+                NAS_path = r"\home\windows\품질관리_장비inspection\01 검사 성적서 관리\2022 검사 성적서\QC SW 테스트"  # NAS 폴더 경로
                 path = NAS_path + '\\' + instrument_SN
                 path = path.replace('\\', '/')
-
-                print(f"path : {path}")
-                print(f"os.getcwd() : {os.getcwd()}")
 
                 if request.FILES.getlist('Appearance_Shock_Watch_Image'):
                     Shock_Watch_files = request.FILES.getlist('Appearance_Shock_Watch_Image')
